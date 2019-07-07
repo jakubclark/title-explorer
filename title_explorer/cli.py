7bb14e675802bc0d06e2f030b8ae44f7391f1439
Kuba Clark <jakub.clark@protonmail.com>
2019-07-07 06:01:23 +0200

    Add Dockerfile

--- title_explorer/cli.py
+++ title_explorer/cli.py
@@ -7,4 +7,4 @@ from .app import init_app
 @click.command()
 def main():
     app = init_app()
-    web.run_app(app, access_log_format='%a %r %s "%{User-Agent}i"')
+    web.run_app(app, host='0.0.0.0', access_log_format='%a %r %s "%{User-Agent}i"')
